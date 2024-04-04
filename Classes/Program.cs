b925c35c6b26a774715050145ffa445ae4c3bde0
JustinBiga <BISENGOJ@GMAIL.COM>
2024-04-04 16:28:17 -0400

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Classes
+﻿using System.Reflection;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of the Car class
+
+          Car myCar = new Car();
+
+            myCar.Make = "Chevy";
+            myCar.Model = "Malibu";
+            myCar.Year = 2016;
+
+            Console.WriteLine($"My Car's info");
+
+            Console.WriteLine("Make: " + myCar.Make);
+            Console.WriteLine("Model: " + myCar.Model);
+            Console.WriteLine("Year: " + myCar.Year);
+
         }
+
     }
 }
